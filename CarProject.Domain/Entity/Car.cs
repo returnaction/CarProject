d61a02ceb30af5f393867d10c15f137c092ea365
returnaction <obergannv@gmail.com>
2024-06-23 08:25:18 -0400

    Add Class car Add enums for Car

---
+++ CarProject.Domain/Entity/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.Domain.Entity
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Model { get; set; }
+        public decimal Speed { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateCreate { get; set; }
+        public TypeCar TypeCar { get; set; }
+    }
+}
---
+++ CarProject.Domain/Enum/TypeCar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.Domain.Enum
+{
+    public enum TypeCar
+    {
+        [Display(Name = "Легкий Автомобиль")]
+        PassangerCar = 0,
+        [Display(Name = "Седан")]
+        Sedan = 1,
+        [Display(Name = "Хэтчбэк")]
+        HatchBack = 2,
+        [Display(Name = "Минивэн")]
+        Minivan = 3,
+        [Display(Name = "Спортивная машина")]
+        SportCar = 4,
+        [Display(Name = "Внедорожник")]
+        Sub = 5
+
+    }
+}
