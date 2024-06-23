49c04549dc7748c35b10d1794f075b79512fb0bf
returnaction <obergannv@gmail.com>
2024-06-23 08:47:04 -0400

    Add Db Context

---
+++ CarProject.DAL/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CarProject.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Car> Cars{ get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
